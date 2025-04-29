f98217c961bf6705d518e149ddd843013d391b87
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-04-29 19:28:23 +0530

    most viewed component

---
+++ app/components/dashboard/MostViewdNews.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import MostViewedNewsCard from "@/common/MostViewedNewsCard";
+
+const newsData = {
+  imageUrl: "https://placehold.co/200x200",
+  title: "Top Trending News Title",
+  description:
+    "This is a brief description of the trending news article, it will be truncated if too long.",
+  views: 1200,
+  onEdit: () => console.log("Edit clicked"),
+  onDelete: () => console.log("Delete clicked"),
+};
+
+const MostViewdNews = () => {
+  return (
+    <div className="space-y-4 border rounded-lg">
+      <MostViewedNewsCard {...newsData} />
+      {/* You can add more news cards here */}
+    </div>
+  );
+};
+
+export default MostViewdNews;
