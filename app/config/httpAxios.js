b7bd89736b5bdd83aa9ca3f631a6d4c4efecd70a
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-04-30 20:42:12 +0530

    update httpaxios and navbar

--- app/config/httpAxios.js
+++ app/config/httpAxios.js
@@ -1,25 +1,18 @@
-// export const httpAxios = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   withCredentials: true,
-// });
-
 import axios from "axios";
+import Cookies from "js-cookie";
 
-// httpAxios.interceptors.request.use((config) => {
-//   const token = Cookies.get("access_token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-// utils/httpAxios.js
-
-const httpAxios = axios.create({
-  baseURL: "https://enews-admin-panel-backend.vercel.app/api", // or your live backend URL
-  withCredentials: true, // ✅ Send cookies
+export const httpAxios = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
-export { httpAxios };
+httpAxios.interceptors.request.use((config) => {
+  const token = Cookies.get("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
