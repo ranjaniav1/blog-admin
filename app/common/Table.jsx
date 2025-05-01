1772bacf8e3fc61669a77cd007968b96cba46484
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-05-01 22:10:47 +0530

    update table and remove extra page

--- app/common/Table.jsx
+++ app/common/Table.jsx
@@ -1,5 +1,7 @@
 "use client";
+import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const Table = ({
   columns,
@@ -8,7 +10,10 @@ const Table = ({
   className = "",
   showAddButton,
   AddButton,
+  linkUrl,
 }) => {
+  const router = useRouter();
+
   return (
     <div
       className={`overflow-x-auto shadow-md rounded-xl border p-4 ${className}`}
@@ -33,7 +38,15 @@ const Table = ({
         </thead>
         <tbody>
           {data.map((item) => (
-            <tr key={item._id || item.slug} className="border-b transition-all duration-150">
+            <tr
+              key={item._id || item.slug}
+              className={`border-b transition-all duration-150 cursor-pointer ${
+                linkUrl ? "hover:bg-gray-100" : ""
+              }`}
+              onClick={() => {
+                if (linkUrl) router.push(`${linkUrl}/${item._id}/${item.slug}`);
+              }}
+            >
               {columns.map((col) => (
                 <td key={col.accessor || col.label} className="px-4 py-3">
                   {col.render
