449dfd4a7c3d2ba43e22c9111dea00218dab36db
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-05-08 15:12:34 +0530

    add setting page with dynamic slug

--- app/admin/settings/page.jsx
+++ app/admin/settings/page.jsx
@@ -1,14 +1,34 @@
-'use client'
+"use client";
 
 import React from "react";
-import { useSettings } from "@/app/hooks/useSettings";
-import WebSettingsForm from "@/app/components/settings/WebSettingsForm";
+import { useSettings } from "@/app/hooks/useWebSettings";
+import SettingsCard from "@/app/common/SettingsCard";
+import { IoSettings, IoLinkSharp  } from "react-icons/io5";
+import { MdOutlineComputer } from "react-icons/md";
+
 
 const Page = () => {
-  const { settings, loading } = useSettings();
-  if (loading) return <p>Loading...</p>;
-  console.log("Settings:", settings);
-  return <WebSettingsForm initialData={settings} />;
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 m-4">
+      <SettingsCard
+        icon={<IoSettings />}
+        title="General Settings"
+        link="/admin/settings/general"
+      />
+      <SettingsCard
+        icon={<IoLinkSharp  />}
+        title="Social Media Settings"
+        link="/admin/settings/social"
+      />
+      <SettingsCard
+        icon={<MdOutlineComputer />}
+        title="Front End Settings"
+        link="/admin/settings/frontend"
+      />
+      {/* Add more cards as needed */}
+    </div>
+  );
 };
 
 export default Page;
