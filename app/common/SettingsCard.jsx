0465b22f3850aa38bcde87cc26cb55b4700bde80
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-05-12 16:48:49 +0530

    final touches

--- app/common/SettingsCard.jsx
+++ app/common/SettingsCard.jsx
@@ -4,14 +4,14 @@ import { TbArrowNarrowRightDashed } from "react-icons/tb";
 export default function SettingsCard({ icon, title, link, isActive = false }) {
   return (
     <Link href={link}>
-      <div className="w-full h-[180px] p-6 rounded-xl my-border transition-all duration-300 cursor-pointer flex flex-col justify-between relative overflow-hidden settings-group card hover:shadow-lg">
+      <div className="w-full h-[180px] p-6 my-rounded transition-all duration-300 cursor-pointer flex flex-col justify-between relative overflow-hidden settings-group card hover:shadow-lg">
         {/* Background fill effect */}
         <div className="animated-bar" />
 
         {/* Icon */}
         <div
           className={`relative text-4xl transition-all duration-300 p-3 rounded-full w-14 h-14 flex items-center justify-center z-10 ${
-            isActive ? "active" : "bg-blue-100 text-blue-600"
+            isActive ? "active" : "bg-blue-100 active-text"
           }  link`}
         >
           {icon}
